9695bcfcb52af433806d21db2658cf202e28ef76
taylor.holmes <taylorh2343@gmail.com>
2022-01-02 18:29:31 -0600

    added api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
+//DOM
 var cityFormEl = document.querySelector("#city-form");
 var nameInputEl = document.querySelector("#city");
+
+//API key
+var apiKey = "ce1c48f71cdacb01f64b2ae634d3fb62";
 
 
 var formSubmitHandler = function(event){
