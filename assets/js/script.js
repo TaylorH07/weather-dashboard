cdd89838dbd44c7fabf6a9385498e9a0d9b22404
taylor.holmes <taylorh2343@gmail.com>
2022-01-02 19:12:09 -0600

    added jquery bootstrap and moment scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,22 @@
 var cityFormEl = document.querySelector("#city-form");
 var nameInputEl = document.querySelector("#city");
 
-//API key
+//API key https://home.openweathermap.org/api_keys
 var apiKey = "ce1c48f71cdacb01f64b2ae634d3fb62";
+
+function getWeather(cityName){
+var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={API key}";
+
+
+};
 
 
 var formSubmitHandler = function(event){
     event.preventDefault();
     console.log(event)
 };
+
+myStorage = window.localStorage;
 
 
 cityFormEl.addEventListener("submit", formSubmitHandler);
