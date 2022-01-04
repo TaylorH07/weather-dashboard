7e1968a40812eb0ff9a4df06afcec710ce71f1c2
taylor.holmes <taylorh2343@gmail.com>
2022-01-03 20:35:53 -0600

    completed show weather container section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 //DOM
 var cityFormEl = document.querySelector("#city-form");
 var nameInputEl = document.querySelector("#city");
-
+var currentWeatherEl = document.querySelector("#current-weather-container");
+var citySearchEl = document.querySelector("#city-search")
 
 var cities = "";
 var cityNames = [];
@@ -46,6 +47,40 @@ function getWeather(cityName){
 
 //display weather in container
 
+var showWeather = function(citySearch, weather){
+
+    currentWeatherEl.textContent="";
+    citySearchEl.classList= citySearch;
+
+    var currentDate = document.createElement("span");
+
+    currentDate.textContent = 
+     "(" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+
+    citySearchEl.appendChild(currentDate);
+
+    var weatherIcons = document.createElement("img")
+        weatherIcons.setAttribute(
+            "src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+    citySearchEl.appendChild(weatherIcons);
+
+    var tempEl = document.createElement("span");
+        tempEl.textContent = "Temperature: " + weather.main.temp + " °F";
+    tempEl.classList = "list-group-item";
+
+    var humidEl = document.createElement("span");
+        humidEl.textContent = "Humidity: " + weather.main.humidity + " %";
+    humidEl.classList = "list-group-item";
+    
+    currentWeatherEl.appendChild(tempEl);
+    currentWeatherEl.appendChild(humidEl);
+    currentWeatherEl.appendChild(windEl);
+    
+    var lon = weather.coord.lon;
+    var lat = weather.coord.lat;
+    getUvIndex(lon, lat);
+
+};
     
 //uv index
 
